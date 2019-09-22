1d64e5e139fff24382d91621d5a9d38f42a93f95
Gerry Mc Donnell <gerrymcdonnell@users.noreply.github.com>
2019-09-22 19:04:43 +0100

    added image to image screen

--- screens/ImageScreen.js
+++ screens/ImageScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text,ScrollView, StyleSheet } from 'react-native';
+import { Image,Text,ScrollView, StyleSheet } from 'react-native';
 
 export default function ImageScreen() {
   return (
@@ -8,6 +8,16 @@ export default function ImageScreen() {
       <Text>
         Image screen
       </Text>
+
+      <Image
+          style={{width: 50, height: 50}}
+          source={{uri: 'https://facebook.github.io/react-native/img/tiny_logo.png'}}
+        />
+
+        <Image source={require('../img/1.jpg')}  style={{width: 400, height: 400}} />
+        <Image source={require('../img/1.jpg')}  style={{width: 400, height: 400}} />
+
+
 
     </ScrollView>
   );
